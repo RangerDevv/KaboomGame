94cd5f6a7509555a79ab81191c67731d80c62727
RangerDevv <66392962+RangerDevv@users.noreply.github.com>
2023-01-20 21:24:58 -0500

    added turning

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import kaboom from "kaboom"
-kaboom()
-
-
-let k = kaboom
+kaboom({
+	background:[215,155,25]
+  })
 
 loadAseprite("Player", "sprites/player/playerFront.svg")
-loadAseprite("Turn", "sprites/player/playerTurn.svg")
+loadAseprite("turnLeft", "sprites/player/playerTurnLeft.svg")
+loadAseprite("turnRight", "sprites/player/playerTurnRight.svg")
+loadAseprite("Back", "sprites/player/playerBack.svg")
 
 const player=add([
     sprite("Player"),
@@ -16,11 +17,48 @@ const player=add([
 	UP = -100,
 	DOWN = 100,
 	area(),
-	solid()
+	solid(),
+	{health : 100}
 ])
-const recto = add([
+const border1 = add([
     rect(width(), 48),
-    pos(0, height() - 48),
+    pos(0, height()-48),
+    outline(4),
+	"rectangle",
+    area(),
+    solid(),
+    color(127, 200, 255),
+])
+const border2 = add([
+    rect(width(), 48),
+    pos(0, 0),
+    outline(4),
+	"rectangle",
+    area(),
+    solid(),
+    color(127, 200, 255),
+])
+const border3 = add([
+    rect(48 , height()),
+    pos(0 , 0),
+    outline(4),
+	"rectangle",
+    area(),
+    solid(),
+    color(127, 200, 255),
+])
+const border4 = add([
+    rect(48 , height()),
+    pos(width() -48, 0),
+    outline(4),
+	"rectangle",
+    area(),
+    solid(),
+    color(127, 200, 255),
+])
+add([
+    rect(48 , height()),
+    pos(width() -48, 0),
     outline(4),
 	"rectangle",
     area(),
@@ -30,18 +68,24 @@ const recto = add([
 
 onKeyDown("left", () => {
     player.move(LEFT, 0)
+	player.use(sprite("turnLeft"))
 })
 onKeyDown("right", () => {
     player.move(RIGHT, 0)
+	player.use(sprite("turnRight"))
 })
 onKeyDown("up", () => {
     player.move(0, UP)
+	player.use(sprite("Back"))
 })
 onKeyDown("down", () => {
     player.move(0, DOWN)
+	player.use(sprite("Player"))
 })
 
 player.onCollide("rectangle", () => {
-    console.log("WOW!")
-	destroyAll("rectangle")
+	shake()
+    console.log(player.health)
+	// destroyAll("rectangle")
 })
+
